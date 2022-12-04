a3aa899c26c8bd2cccc07be2996bc6ef51e8e6bf
hturner08 <hmturner@mit.edu>
2022-12-04 03:26:29 -0500

    weird bug, testing with commit

--- cmod_matlab/matlab-utils/visualization_code/plot_ip_error.m
+++ cmod_matlab/matlab-utils/visualization_code/plot_ip_error.m
@@ -1,50 +1,71 @@
-if (exist('time','var')==0 || exist('time_until_disrupt','var')==0 || ...
-    exist('ip','var')==0 || exist('ip_error','var')==0);
-  fprintf(1,['Call the routine ''retrieve_all_params'' before calling ' ...
-             'this routine.\n']);
-  return;
+db = set_database('logbook');
+result = fetch(db,['select shot,time_until_disrupt,ip,ip_error,' ...
+                'dipprog_dt,time from disruption_warning order by ' ...
+                'shot,time']);
+close(db);
+
+shots = int32(cell2mat(result(:,1)));
+tdis = cell2mat(result(:,2));
+ip = cell2mat(result(:,3));
+iperr = cell2mat(result(:,4));
+didt = cell2mat(result(:,5));
+times = cell2mat(result(:,6));
+
+% Take account of the Ip polarity.  (Maybe I should add a 'polarity'
+% parameter to the database?)
+
+unique_shots = unique(shots);
+for i = 1:length(unique_shots);
+  indices = find(shots == unique_shots(i));
+  polarity = sign(sum(ip(indices)));
+  iperr(indices) = iperr(indices) * polarity;
+  didt(indices) = didt(indices) * polarity;
 end;
 
-get_ip_sign;
-get_flattop_indices;
+flattop  = find(didt == 0);
+rampdown = find(didt  < 0);
+rampup   = find(didt  > 0);
 
-figure('Name', ' ', 'NumberTitle','off','Menubar','none','Toolbar','none')
-plot(-time_until_disrupt(indices_flattop), ...         
-  ip_error(indices_flattop)/1.e3.*ipsign(indices_flattop),'.');
-xlim([-1,0]);
-ylim([-700,200]);
-xlabel('Time before disrupt [s]', 'FontSize', 14);
-ylabel('Ip error (processed for sign of Ip) [kA]', 'FontSize', 14);
-title('Ip error vs time before disrupt (flattop ONLY)', 'FontSize', 16);
- 
-set(gcf, 'PaperPositionMode', 'auto');
-%print -dpng iperror_plot_1.png;
+figure('menubar','none','toolbar','none');
+plot(-tdis(flattop)*1.e3, iperr(flattop)/1.e3, '.b');
+xlim([-350,0]);
+ylim([-100,100]);
+xlabel('Time before disruption [ms]', 'fontsize', 12);
+ylabel('Ip error ( = Ip - Ip_{programmed}) [kA]', 'fontsize', 12);
+title('During flattop', 'fontsize', 14);
+hold on;
+plot([-350,0],[0,0],'g','linewidth',2);
+hold off;
 
-figure('Name', ' ', 'NumberTitle','off','Menubar','none','Toolbar','none')
-plot(-smear(time_until_disrupt(indices_flattop))*1.e3, ...         
-  ip_error(indices_flattop)/1.e3.*ipsign(indices_flattop),'.');
-xlim([-50,0]);
-ylim([-700,200]);
-xlabel('Time before disrupt [ms]', 'FontSize', 14);
-ylabel('Ip error (processed for sign of Ip) [kA]', 'FontSize', 14);
-title('Ip error vs time before disrupt (flattop ONLY)', 'FontSize', 16);
- 
-set(gcf, 'PaperPositionMode', 'auto');
-%print -dpng iperror_plot_2.png;
+figure('menubar','none','toolbar','none');
+plot(-tdis(rampdown)*1.e3, iperr(rampdown)/1.e3, '.b');
+xlim([-350,0]);
+ylim([-100,100]);
+xlabel('Time before disruption [ms]', 'fontsize', 12);
+ylabel('Ip error ( = Ip - Ip_{programmed}) [kA]', 'fontsize', 12);
+title('During rampdown', 'fontsize', 14);
+hold on;
+plot([-350,0],[0,0],'g','linewidth',2);
+hold off;
 
-figure('Name', ' ', 'NumberTitle','off','Menubar','none','Toolbar','none')
+figure('menubar','none','toolbar','none');
+plot(-tdis(rampup)*1.e3, iperr(rampup)/1.e3, '.b');
+xlim([-350,0]);
+ylim([-100,100]);
+xlabel('Time before disruption [ms]', 'fontsize', 12);
+ylabel('Ip error ( = Ip - Ip_{programmed}) [kA]', 'fontsize', 12);
+title('During rampup', 'fontsize', 14);
+hold on;
+plot([-350,0],[0,0],'g','linewidth',2);
+hold off;
 
-indices_nodisrupt = find(isnan(time_until_disrupt));
-indices_nodisrupt_flattop = intersect(indices_nodisrupt, indices_flattop);
-
-plot(time(indices_nodisrupt_flattop), ...         
-  ip_error(indices_nodisrupt_flattop)/1.e3 .* ...
-  ipsign(indices_nodisrupt_flattop),'.');
-xlim([0,2]);
-ylim([-700,200]);
-xlabel('Time [s]', 'FontSize', 14);
-ylabel('Ip error (processed for sign of Ip) [kA]', 'FontSize', 14);
-title('Ip error for non-disruptive shots (flattop ONLY)', 'FontSize', 16);
- 
-set(gcf, 'PaperPositionMode', 'auto');
-%print -dpng iperror_plot_3.png;
+figure('menubar','none','toolbar','none');
+plot(times, iperr/1.e3, '.b');
+xlim([0,2.3]);
+ylim([-100,100]);
+xlabel('Time during shot [s]', 'fontsize', 12);
+ylabel('Ip error ( = Ip - Ip_{programmed}) [kA]', 'fontsize', 12);
+%title('During rampup', 'fontsize', 14);
+hold on;
+plot([0,2.3],[0,0],'g','linewidth',2);
+hold off;
