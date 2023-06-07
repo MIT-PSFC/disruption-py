2fd0d6b05fc8128d118f6094cf5e41efaf0bbab2
+Herbert Turner <turnerh@saga.cluster>
2023-06-07 07:18:02 -0700

    Refactored script hleper functions into ml module.

--- disruption_py/ml/preprocessing.py
+++ disruption_py/ml/preprocessing.py
@@ -34,7 +34,7 @@ PAPER_COLS = [
     'li',
     'Wmhd',
     'kappa',
-    # 'squareness'
+    'squareness'
 ]
 DERIVED_PAPER_COLS = [
     'ip-exp-10-none',
@@ -69,39 +69,39 @@ def get_dataset_df(data_source=2, cols=DEFAULT_COLS, efit_tree=None, shot_ids=No
     if tokamak not in ['d3d', 'cmod']:
         raise NotImplementedError(
             "Currently only support DIII-D and Alcator C-MOD data retrieval")
-    else:
-        tokamak_handler = TOKAMAKS[tokamak]()
+    tokamak_str = tokamak
+    if tokamak_str == 'd3d':
+        tokamak = TOKAMAKS[tokamak]()
     timebase_signal = kwargs.get('timebase_signal', None)
     populate = kwargs.get('populate', 'default')
     label = kwargs.get('label', 'none')
     if shot_ids is None:
         random_state = kwargs.get('random_state', 8808)
-        shot_ids = tokamak_handler.get_disruption_table_shotlist()['shot']
+        shot_ids = tokamak.get_disruption_table_shotlist()['shot']
         random.Random(random_state).shuffle(shot_ids)
         shot_ids = shot_ids[:1200]
     if data_source == 0:
-        shots = [tokamak_handler.get_shot(shot_id, efit_tree) for shot_id in shot_ids]
+        shots = [tokamak.get_shot(shot_id, efit_tree) for shot_id in shot_ids]
         dataset_df = pd.concat([shot.data for shot in shots])
     elif data_source == 1:
         raise NotImplementedError
     elif data_source == 2:
-        dataset_df = tokamak_handler.get_shot_data(shot_ids, cols)
+        dataset_df = tokamak.get_shot_data(shot_ids, cols)
     elif data_source == 3:
         shots = []
         timebase_signal = kwargs.get('timebase_signal', None)
         for shot_id in shot_ids:
             try:
-                if tokamak == 'd3d':
+                if tokamak_str == 'd3d':
                     if efit_tree is None:
-                        shots.append(D3DShot(shot_id, tokamak_handler.get_efit_tree(
-                            shot_id), disruption_time=tokamak_handler.get_disruption_time(shot_id), timebase_signal=timebase_signal, populate=populate))
+                        shots.append(D3DShot(shot_id, tokamak.get_efit_tree(
+                            shot_id), disruption_time=tokamak.get_disruption_time(shot_id), timebase_signal=timebase_signal, populate=populate))
                     else:
-                        shots.append(D3DShot(shot_id, efit_tree, disruption_time=tokamak_handler.get_disruption_time(shot_id),
+                        shots.append(D3DShot(shot_id, efit_tree, disruption_time=tokamak.get_disruption_time(shot_id),
                                              timebase_signal=timebase_signal, populate=populate))
-                elif tokamak == 'cmod':
-                    #shots.append(CModShot("cmod",shot_id=shot_id))
-                    shots.append(CModShot(shot_id=shot_id, disruption_time=tokamak_handler.get_disruption_time(shot_id)))
-                LOGGER.info(f"[Shot {shot_id}]:Generated shot object, {idx} of {len(shot_ids)} ({percent_complete:.1f}% percent complete)' ")
+                elif tokamak_str == 'cmod':
+                    shots.append(CModShot("cmod",shot_id=shot_id))
+                LOGGER.info(f"[Shot {shot_id}]:Generated shot object")
             except Exception as e:
                 LOGGER.info(f"[Shot {shot_id}]:Failed to generate shot object")
                 # exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -190,7 +190,7 @@ def parse_feature_cols(feature_str):
         all_cols = pd.read_csv(
             feature_str, header=None).iloc[:, 0].values
     else:
-        all_cols = [feature.strip()
+        all_cols = [feature.trim()
                     for feature in feature_str.split(",")]
     feature_cols = []
     derived_feature_cols = []
