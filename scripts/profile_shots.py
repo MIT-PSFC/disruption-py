f8a6433f7f3550e27e00d1cfc69fe0cd41c45768
hturner08 <hmturner@mit.edu>
2023-06-15 23:35:30 -0400

    Added profiling on cmod shots

--- scripts/profile_shots.py
+++ scripts/profile_shots.py
@@ -20,20 +20,16 @@ def calibrate_profiler(pr):
     
 def main(args):
     shot_type = get_shot_id_type(args.shot)
-    pr = profile.Profile()
-    pr.bias = calibrate_profiler(pr)
     if shot_type == CModShot:
         cmod = create_cmod_handler()
-        pr.run('CModShot(args.shot)')
+        loop = lambda : CModShot(args.shot)
     elif shot_type == D3DShot:
         d3d = create_d3d_handler()
-        pr.run('D3DShot(args.shot, args.efit)')
+        loop = lambda : D3DShot(args.shot, args.efit)
     else:
         raise ValueError(f"Shot {args.shot} not found in CMOD or D3D")
-    p = pstats.Stats(pr)
-    p.sort_stats(SortKey.CUMULATIVE).print_stats(50)
-    p.sort_stats(SortKey.TIME).print_stats(50)
-    pr.dump_stats(f"shot_{args.shot}_profileX{args.num_calls}")
+    for i in range(args.num_calls):
+        loop()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Profile a Shot')
