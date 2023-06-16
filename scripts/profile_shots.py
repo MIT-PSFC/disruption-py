c8785ecca320a395bdfa6b7986f08fad1a4e42f9
hturner08 <hmturner@mit.edu>
2023-06-15 23:35:30 -0400

    Added profiling on cmod shots

--- scripts/profile_shots.py
+++ scripts/profile_shots.py
@@ -10,7 +10,8 @@ import pstats
 import argparse
 from pstats import SortKey
 
-from disruption_py.shots import D3DShot
+from disruption_py.shots import D3DShot, CModShot, get_shot_id_type
+from disruption_py.database import create_d3d_handler, create_cmod_handler
 
 
 def calibrate_profiler(pr):
@@ -18,12 +19,20 @@ def calibrate_profiler(pr):
 
 
 def main(args):
+    shot_type = get_shot_id_type(args.shot)
+    if shot_type == CModShot:
+        cmod = create_cmod_handler()
+        loop = lambda : CModShot(args.shot)
+    elif shot_type == D3DShot:
+        d3d = create_d3d_handler()
+        loop = lambda : D3DShot(args.shot, args.efit)
+    else:
+        raise ValueError(f"Shot {args.shot} not found in CMOD or D3D")
     for i in range(args.num_calls):
-        D3DShot(args.shot, args.efit)
-
+        loop()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Profile D3DShot')
+    parser = argparse.ArgumentParser(description='Profile a Shot')
     parser.add_argument('--shot', type=int, default=175552)
     parser.add_argument('--efit', type=str, default='EFIT05')
     parser.add_argument('--num_calls', type=int, default=1)
