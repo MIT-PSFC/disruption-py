16a589f7b8a5af7cd87bf4d7992439eb8b76b14e
ZanderKeith <zkeith@mit.edu>
2023-06-28 09:37:58 -0400

    Requested comments in shot.py

--- disruption_py/shots/shot.py
+++ disruption_py/shots/shot.py
@@ -222,6 +222,9 @@ class Shot:
         # What is the intended logic here? 
         # This is *incredibly* confusing
         # TODO: Add comments to explain what is going on here
+        # What is the intended logic here? 
+        # This is *incredibly* confusing
+        # TODO: Add comments to explain what is going on here
         if not already_populated:
             if self.data is None:
                 self.data = pd.DataFrame()
@@ -237,12 +240,15 @@ class Shot:
         for method_name in dir(self):
             method = getattr(self, method_name)
             if callable(method) and hasattr(method, 'populate'):
-                if tags is not None and not bool(set(method.tags).intersection(tags)):
-                    print(f"[Shot {self._shot_id}]:Skipping {method_name}")
+                if (methods is not None and method_name not in methods):
                     continue
-                if methods is not None and method_name not in methods:
-                    print(f"[Shot {self._shot_id}]:Skipping {method_name}")
-                    continue
+                try:
+                    # This if statement throws an exception if tags is 'None', which is the default
+                    if not bool(set(method.tags).intersection(tags)):
+                        continue
+                except:
+                    pass
+                
                 try:
                     # This if statement throws an exception if tags is 'None', which is the default
                     if not bool(set(method.tags).intersection(tags)):
