46e3ef3c34a3c8219dc6cd64422ec8ea049b5d07
ZanderKeith <zkeith@mit.edu>
2023-06-29 12:00:50 -0400

    Added scikit-learn to requirements.txt, updated .gitignore to stop tracking .venv, moved tests up a directory to enable test discovery by unittest

--- test/test_cmod.py
+++ test/test_cmod.py
@@ -1,10 +1,25 @@
 """Unit tests for workflows involving get_dataset_df() for obtaining CMOD data.
 
 Expects to be run on the MFE workstations.
+Expects MDSplus to be installed and configured.
+Expects SQL credentials to be configured.
 """
 import unittest
 
 from disruption_py.ml.preprocessing import get_dataset_df
+
+# Shot list used for testing
+# Mix of disruptive and non-disruptive shots present in SQL and MDSplus
+TEST_SHOTS = [1150805012,   # Flattop Disruption
+            1150805013,     # No Disruption
+            1150805014,     # No Disruption
+            1150805015,     # Rampdown Disruption
+            1150805016,     # Rampdown Disruption
+            1150805017,     # Rampdown Disruption
+            1150805019,     # Rampdown Disruption
+            1150805020,     # Rampdown Disruption
+            1150805021,     # Rampdown Disruption
+            1150805022]     # Flattop Disruption
 
 class TestSQL(unittest.TestCase):
     """Tests for the using the SQL table as a data source."""
@@ -12,7 +27,9 @@ class TestSQL(unittest.TestCase):
     def test_get_all_columns(self):
         """Ensure there is no error when getting all columns from the SQL table for several shots.
         """
-        self.assertTrue(False)
+        sql_df = get_dataset_df(tokamak='cmod', shot_ids=TEST_SHOTS, data_source=2)
+
+        self.assertIsNotNone(sql_df)
 
 
 
