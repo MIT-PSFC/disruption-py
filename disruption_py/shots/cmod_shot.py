Fix units for R0 in H98

disruption_py/shots/cmod_shot.py
@@ -1548,12 +1548,11 @@ class CModShot(Shot):
         return CModShot.get_edge_parameters(self._times, p_Te, p_ne)
 
     @staticmethod
-    def get_H98(times, tau, t_tau):
-
-
+    def get_H98():
         pass
 
-    @parameter_method
+    # TODO: Finish
+    @parameter_method()
     def _get_H98(self):
         """Prepare to compute H98 by getting tau_E
         
@@ -1581,26 +1580,21 @@ class CModShot(Shot):
         btor = btor - np.mean(btor[baseline_indices])
         btor = np.abs(interp1(t_mag, btor, self._times))
         
-        #For H98 computation, use 
-        R0 = 0.68 #[m] geometric major radius of C-Mod
-        A = 2 #[amu] assume Deuterium, with atomic mass 2
-
-        #Get variables need to comptue confinement time
-        ip = np.abs(ip_df.ip)/1e6 # [A] -> [MA]
-        n_e = density_df.n_e/1e19 # [m^-3] -> [10^19 m^-3]
+        ip = np.abs(ip_df.ip)/1.e6 # [A] -> [MA]
+        n_e = density_df.n_e/1.e19 # [m^-3] -> [10^19 m^-3]
         p_input = powers_df.p_input/1.e6 # [W] -> [MW]
         dWmhd_dt = efit_df.dWmhd_dt/1.e6 # [W] -> [MW]
         Wmhd = efit_df.Wmhd/1.e6 # [J] -> [MJ]
+        R0 = efit_df.R0/100 # [cm] -> [m]
         #Estimate confinement time
         tau = Wmhd/(p_input - dWmhd_dt)
         
-        #Compute 1998 tau_E scaling
-        tau_98 = .0562*(n_e**0.41)*(A**0.19)*(ip**0.93)*(R0**1.39) * \
+        #Compute 1998 tau_E scaling, taking A (atomic mass) = 2
+        tau_98 = .0562*(n_e**0.41)*(2**0.19)*(ip**0.93)*(R0**1.39) * \
                 (efit_df.a_minor**0.58)*(efit_df.kappa**0.78)*(btor**0.15)*(p_input**-0.69)
         H98 = tau/tau_98
 
         return pd.DataFrame({"H98": H98})
-        #return CModShot.get_H98(self._times, tau)
 
 
 if __name__ == '__main__':