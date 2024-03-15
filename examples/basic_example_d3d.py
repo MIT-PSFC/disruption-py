466aa60616e578969551689ce44964e6e8d45686
Joshua Lorincz <joshlor@mit.edu>
2024-03-15 02:28:50 -0700

    Add d3d basic example

---
+++ examples/basic_example_d3d.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("/home/lorinczj/disruption-py")
+
+import logging
+from disruption_py.settings.log_settings import LogSettings
+from disruption_py.handlers.d3d_handler import D3DHandler
+from disruption_py.databases.dummy_database import DummyDatabase
+from disruption_py.settings.shot_settings import ShotSettings
+
+
+cmod_handler = D3DHandler()
+shot_settings = ShotSettings(
+    # uses the efit timebase when returning data 
+    set_times_request="ip",
+    
+    # run all available methods
+    run_tags=["all"],
+    
+    log_settings=LogSettings(
+        console_log_level=logging.DEBUG,
+	),
+)
+shot_data = cmod_handler.get_shots_data(
+    # Retrieve data for the desired shots
+    shot_ids_request=[161228],
+    shot_settings=shot_settings,
+    
+    # automatically stream retrieved data to a csv file by passing in a file path ending in .csv
+    output_type_request="/cscratch/lorinczj/data.csv",
+    
+    num_processes = 1
+)
