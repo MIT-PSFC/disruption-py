eeee753309b25ce9e55ac38d4343703a7258d867
Alex R. Saperstein <saperstein@psfc.mit.edu>
2024-05-08 15:32:58 -0400

    switched from 'analysis'-->'efit18' tree and added some additional tests

--- examples/wm_tests/test_data_fetching.py
+++ examples/wm_tests/test_data_fetching.py
@@ -3,15 +3,14 @@ Be sure to run this script using 'disruption-python' instead of 'python' to use
 the correct environment.
 '''
 
-from disruption_py.databases.cmod_database import CModDatabase
 from disruption_py.handlers import CModHandler
 from disruption_py.settings import ShotSettings, LogSettings
 import logging
 import numpy as np
-import time
 import matplotlib.pyplot as plt
-import pickle
 import json
+import pandas as pd
+import os
 
 # load all possile feature-sets and shot-lists
 with open("feature_sets.json","r") as f:
@@ -20,60 +19,70 @@ with open("feature_sets.json","r") as f:
 with open("shot_lists.json","r") as f:
     shot_lists = json.load(f)
 
-# choose feature-set and shot-list
-shot_list = np.array(shot_lists['random100'])
-features = feature_sets['mhd']
+shot_list_name = 'random100'
+feature_set_name = 'mhd'
 
-print(shot_list)
+# choose feature-set and shot-list
+shot_list = np.array(shot_lists[shot_list_name])
+features = feature_sets[feature_set_name]
+
+#print(shot_list)
 
 # pull data
 
-start_time = time.time()
+if os.path.exists(f"{shot_list_name}-{feature_set_name}.csv"):
+    shot_data = pd.read_csv(f"{shot_list_name}-{feature_set_name}.csv")
+elif os.path.exists(f"{shot_list_name}-{feature_set_name}.h5"):
+    shot_data = pd.read_hdf(f"{shot_list_name}-{feature_set_name}.hdf")
+else:
+    handler = CModHandler(mds_connection_str='alcdata-archives')   # {'alcdata-archives': MDSplus, 'DoNotConnect': hsds}
 
-handler = CModHandler()
+    shot_settings = ShotSettings(
+        # logging
+        log_settings=LogSettings(
+            log_file_path=None,
+            file_log_level=logging.WARNING,
+            log_file_write_mode="w",
+            log_to_console=True,
+            console_log_level=logging.WARNING,
+            use_custom_logging=False,
+        ),
 
-shot_settings = ShotSettings(
-    # logging
-    log_settings=LogSettings(
-        log_file_path=None,
-        file_log_level=logging.WARNING,
-        log_file_write_mode="w",
-        log_to_console=True,
-        console_log_level=logging.WARNING,
-        use_custom_logging=False,
-    ),
-    
-    # data settings
-    existing_data_request=None,
-    efit_tree_name="analysis",
-    
-    # method selection
-    run_methods=[],
-    run_tags=[],    
-    run_columns=features,   # add list of features here
-    only_requested_columns=True,
-    shot_data_requests=[],
-    
-    # timebase settings
-    set_times_request = "efit", # use efit timebase
-    signal_domain = "full",
-    use_existing_data_timebase = False,
-    interpolation_method= "linear",
-)
+        # data settings
+        existing_data_request=None,
+        efit_tree_name="efit18",
+        
+        # method selection
+        run_methods=[],
+        run_tags=[],    
+        run_columns=features,   # add list of features here
+        only_requested_columns=True,
+        shot_data_requests=[],
+        
+        # timebase settings
+        set_times_request = "efit", # use efit timebase
+        signal_domain = "full",
+        use_existing_data_timebase = False,
+        interpolation_method= "linear",
+    )
 
-shot_data = handler.get_shots_data(
-    shot_ids_request=shot_list,
-    shot_settings=shot_settings,
-    output_type_request = "dataframe", # output a list of dataframes
-    num_processes=20,
-)
+    shot_data = handler.get_shots_data(
+        shot_ids_request=shot_list,
+        shot_settings=shot_settings,
+        output_type_request = "dataframe", # output a list of dataframes
+        num_processes=20,
+    )
 
-print()
-print(shot_data.keys())
+    shot_data.to_csv(f"{shot_list_name}-{feature_set_name}.csv")
+    shot_data.to_hdf(f"{shot_list_name}-{feature_set_name}.h5", key='data')
 
-end_time = time.time()
+print(shot_data)
 
-print(f"Elapsed time: {end_time-start_time} seconds")
+# check csv and h5 have the same data
+csv_data = pd.read_csv(f"{shot_list_name}-{feature_set_name}.csv")
+h5_data = pd.read_hdf(f"{shot_list_name}-{feature_set_name}.h5")
+
+diff = csv_data - h5_data
 
 # check data
 for f in features:
