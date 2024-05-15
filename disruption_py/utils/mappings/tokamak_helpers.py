d70c7ae726ebf0a8ec529b51013d14875582f575
Joshua Lorincz <26237613+lajz@users.noreply.github.com>
2024-05-15 17:40:07 -0400

    Single dictionaries for test constants

--- disruption_py/utils/mappings/tokamak_helpers.py
+++ disruption_py/utils/mappings/tokamak_helpers.py
@@ -3,7 +3,7 @@ from disruption_py.databases import D3DDatabase, CModDatabase
 from disruption_py.utils.mappings.tokamak import Tokamak
 from disruption_py.handlers.cmod_handler import CModHandler
 from disruption_py.handlers.d3d_handler import D3DHandler
-from disruption_py.utils.constants import CMOD_EXPECTED_FAILURE_COLUMNS, CMOD_TEST_COLUMNS, CMOD_TEST_SHOTS, D3D_EXPECTED_FAILURE_COLUMNS, D3D_TEST_COLUMNS, D3D_TEST_SHOTS
+from disruption_py.utils.constants import EXPECTED_FAILURE_COLUMNS, TEST_COLUMNS, TEST_SHOTS
 
 def get_tokamak_from_shot_id(shot_id):
     if isinstance(shot_id, str):
@@ -53,21 +53,11 @@ def get_tokamak_database(tokamak : Tokamak):
         raise ValueError("Tokamak {} not supported for this test".format(tokamak))
     
 def get_tokamak_test_expected_failure_columns(tokamak : Tokamak):
-    if tokamak == Tokamak.CMOD:
-        return CMOD_EXPECTED_FAILURE_COLUMNS
-    elif tokamak == Tokamak.D3D:
-        return D3D_EXPECTED_FAILURE_COLUMNS
-    else:
-        raise ValueError("Tokamak {} not supported for this test".format(tokamak))
+    return EXPECTED_FAILURE_COLUMNS.get(tokamak.value)
 
 
 def get_tokamak_test_shot_ids(tokamak : Tokamak) -> list[int]:
-    if tokamak == Tokamak.CMOD:
-        shot_id_dict = CMOD_TEST_SHOTS
-    elif tokamak == Tokamak.D3D:
-        shot_id_dict =  D3D_TEST_SHOTS
-    else:
-        raise ValueError("Tokamak {} not supported for this test".format(tokamak))
+    shot_id_dict = TEST_SHOTS.get(tokamak.value)
     
     if "GITHUB_ACTIONS" in os.environ:
         shot_id_dict = {key: value for key, value in shot_id_dict.items() if "_fast" in key}
@@ -76,9 +66,4 @@ def get_tokamak_test_shot_ids(tokamak : Tokamak) -> list[int]:
 
 
 def get_tokamak_test_columns(tokamak : Tokamak):
-    if tokamak == Tokamak.CMOD:
-        return CMOD_TEST_COLUMNS
-    elif tokamak == Tokamak.D3D:
-        return D3D_TEST_COLUMNS
-    else:
-        raise ValueError("Tokamak {} not supported for this test".format(tokamak))
+    return TEST_COLUMNS.get(tokamak.value)
