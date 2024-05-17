08f84006a099837be581d130afdf6ad690299a08
gtrevisan <gtrevisan@users.noreply.github.com>
2024-05-17 09:32:57 -0400

    black! disruption_py/utils/mappings/tokamak_helpers.py

--- disruption_py/utils/mappings/tokamak_helpers.py
+++ disruption_py/utils/mappings/tokamak_helpers.py
@@ -3,7 +3,12 @@ from disruption_py.databases import D3DDatabase, CModDatabase
 from disruption_py.utils.mappings.tokamak import Tokamak
 from disruption_py.handlers.cmod_handler import CModHandler
 from disruption_py.handlers.d3d_handler import D3DHandler
-from disruption_py.utils.constants import EXPECTED_FAILURE_COLUMNS, TEST_COLUMNS, TEST_SHOTS
+from disruption_py.utils.constants import (
+    EXPECTED_FAILURE_COLUMNS,
+    TEST_COLUMNS,
+    TEST_SHOTS,
+)
+
 
 def get_tokamak_from_shot_id(shot_id):
     if isinstance(shot_id, str):
@@ -12,16 +17,15 @@ def get_tokamak_from_shot_id(shot_id):
         # math.log10 is faster and safer for large numbers but we assume shot_id is relatively small
         shot_len = len(str(shot_id))
     else:
-        raise ValueError(
-            f"shot_id must be a string or integer, not {type(shot_id)}")
+        raise ValueError(f"shot_id must be a string or integer, not {type(shot_id)}")
 
     if shot_len == 6:
         return Tokamak.D3D
     elif shot_len == 10:
         return Tokamak.CMOD
     else:
-        raise NotImplementedError(
-            f"Unable to handle shot_id of length {shot_len}")
+        raise NotImplementedError(f"Unable to handle shot_id of length {shot_len}")
+
 
 def get_tokamak_from_environment():
     if "DISPY_TOKAMAK" in os.environ:
@@ -32,38 +36,44 @@ def get_tokamak_from_environment():
         return Tokamak.D3D
     return None
 
-def get_database_for_shot_id(shot_id : int):
+
+def get_database_for_shot_id(shot_id: int):
     tokamak = get_tokamak_from_shot_id(shot_id)
     return get_tokamak_database(tokamak)
 
-def get_tokamak_handler(tokamak : Tokamak):
+
+def get_tokamak_handler(tokamak: Tokamak):
     if tokamak is Tokamak.CMOD:
         return CModHandler()
     elif tokamak is Tokamak.D3D:
         return D3DHandler()
     else:
         raise ValueError("Tokamak {} not supported for this test".format(tokamak))
-    
-def get_tokamak_database(tokamak : Tokamak):
+
+
+def get_tokamak_database(tokamak: Tokamak):
     if tokamak == Tokamak.CMOD:
         return CModDatabase.default()
     elif tokamak == Tokamak.D3D:
         return D3DDatabase.default()
     else:
         raise ValueError("Tokamak {} not supported for this test".format(tokamak))
-    
-def get_tokamak_test_expected_failure_columns(tokamak : Tokamak):
+
+
+def get_tokamak_test_expected_failure_columns(tokamak: Tokamak):
     return EXPECTED_FAILURE_COLUMNS.get(tokamak.value)
 
 
-def get_tokamak_test_shot_ids(tokamak : Tokamak) -> list[int]:
+def get_tokamak_test_shot_ids(tokamak: Tokamak) -> list[int]:
     shot_id_dict = TEST_SHOTS.get(tokamak.value)
-    
+
     if "GITHUB_ACTIONS" in os.environ:
-        shot_id_dict = {key: value for key, value in shot_id_dict.items() if "_fast" in key}
-        
+        shot_id_dict = {
+            key: value for key, value in shot_id_dict.items() if "_fast" in key
+        }
+
     return list(shot_id_dict.values())
 
 
-def get_tokamak_test_columns(tokamak : Tokamak):
+def get_tokamak_test_columns(tokamak: Tokamak):
     return TEST_COLUMNS.get(tokamak.value)
