a0e61448b040ec9812ff131271bd53d11dbd8cd6
gtrevisan <gtrevisan@users.noreply.github.com>
2024-05-17 09:32:50 -0400

    black! disruption_py/settings/__init__.py

--- disruption_py/settings/__init__.py
+++ disruption_py/settings/__init__.py
@@ -3,5 +3,9 @@ from .shot_data_request import ShotDataRequest, ShotDataRequestParams
 from .existing_data_request import ExistingDataRequest, ExistingDataRequestParams
 from .shot_settings import ShotSettings
 from .set_times_request import SetTimesRequest, SetTimesRequestParams
-from .output_type_request import OutputTypeRequest, ResultOutputTypeRequestParams, FinishOutputTypeRequestParams
+from .output_type_request import (
+    OutputTypeRequest,
+    ResultOutputTypeRequestParams,
+    FinishOutputTypeRequestParams,
+)
 from .log_settings import LogSettings
