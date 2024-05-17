169db8e47160a5feb1353fc4732eab59403bae18
gtrevisan <gtrevisan@users.noreply.github.com>
2024-05-17 09:32:55 -0400

    black! disruption_py/shots/parameter_methods/d3d/built_in.py

--- disruption_py/shots/parameter_methods/d3d/built_in.py
+++ disruption_py/shots/parameter_methods/d3d/built_in.py
@@ -1,5 +1,11 @@
-from disruption_py.shots.parameter_methods.d3d.basic_parameter_methods import BasicD3DRequests
-from disruption_py.shots.parameter_methods.d3d.efit_parameter_methods import D3DEfitRequests
+from disruption_py.shots.parameter_methods.d3d.basic_parameter_methods import (
+    BasicD3DRequests,
+)
+from disruption_py.shots.parameter_methods.d3d.efit_parameter_methods import (
+    D3DEfitRequests,
+)
+
 D3D_DEFAULT_SHOT_DATA_REQUESTS = [
-    D3DEfitRequests(), BasicD3DRequests(),
+    D3DEfitRequests(),
+    BasicD3DRequests(),
 ]
