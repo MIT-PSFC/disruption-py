8a7e6308deb7d5e38dd98903d888860769b2b307
Joshua Lorincz <26237613+lajz@users.noreply.github.com>
2024-06-06 12:40:27 -0400

    Skip cmod fixtures on d3d

--- tests/test_cmod_writeback.py
+++ tests/test_cmod_writeback.py
@@ -5,15 +5,11 @@ import pandas as pd
 import pytest
 
 from disruption_py.handlers.cmod_handler import CModHandler
+from disruption_py.handlers.handler import Handler
 from disruption_py.settings.output_type_request import SQLOutputRequest
 from disruption_py.settings.shot_settings import ShotSettings
 from disruption_py.utils.constants import BASE_PROTECTED_COLUMNS
 from disruption_py.utils.utils import without_duplicates
-
-TEST_SHOTS = [
-    1150805012,  # Flattop Disruption
-    1150805013,  # No Disruption
-]
 
 TABLE_NAME = "disruption_warning_test"
 FIRST_ITERATION_COLUMNS = BASE_PROTECTED_COLUMNS + ["beta_p"]
@@ -22,19 +18,18 @@ ALL_ITERATION_COLUMNS = without_duplicates(
     FIRST_ITERATION_COLUMNS + SECOND_ITERATION_COLUMNS
 )
 
-@pytest.fixture(scope="class")
-def cmod_handler():
-    return CModHandler()
 
 @pytest.fixture(scope="class")
-def initial_mdsplus_data(cmod_handler: CModHandler, shotlist) -> Dict:
+def initial_mdsplus_data(handler: Handler, shotlist) -> Dict:
+    if not os.path.exists("/fusion/projects/disruption_warning"):
+        pytest.skip("Skipping test on DIII-D")
     shot_settings = ShotSettings(
         set_times_request="efit",
         efit_tree_name="efit18",
         run_columns=FIRST_ITERATION_COLUMNS,
         only_requested_columns=True,
     )
-    shot_data, _ = cmod_handler.get_shots_data(
+    shot_data, _ = handler.get_shots_data(
         shot_ids_request=shotlist,
         shot_settings=shot_settings,
         output_type_request=["dataframe", SQLOutputRequest(table_name=TABLE_NAME)],
@@ -54,10 +49,10 @@ def assert_frame_equal_unordered(df1: pd.DataFrame, df2: pd.DataFrame):
 @pytest.mark.skipif(
     os.path.exists("/fusion/projects/disruption_warning"), reason="on DIII-D"
 )
-def test_update_data(cmod_handler: CModHandler, shotlist, initial_mdsplus_data) -> Dict:
+def test_update_data(handler: Handler, shotlist, initial_mdsplus_data) -> Dict:
     # Test initial database readback
-    cmod_database = cmod_handler.database
-    result = cmod_database.get_shots_data(shotlist, sql_table=TABLE_NAME)
+    database = handler.database
+    result = database.get_shots_data(shotlist, sql_table=TABLE_NAME)
     assert_frame_equal_unordered(
         result[FIRST_ITERATION_COLUMNS], initial_mdsplus_data[FIRST_ITERATION_COLUMNS]
     )
@@ -69,7 +64,7 @@ def test_update_data(cmod_handler: CModHandler, shotlist, initial_mdsplus_data) 
         run_columns=ALL_ITERATION_COLUMNS,
         only_requested_columns=True,
     )
-    shot_data, _ = cmod_handler.get_shots_data(
+    shot_data, _ = handler.get_shots_data(
         shot_ids_request=shotlist,
         shot_settings=shot_settings,
         output_type_request=[
@@ -80,8 +75,7 @@ def test_update_data(cmod_handler: CModHandler, shotlist, initial_mdsplus_data) 
         ],
         num_processes=1,
     )
-    cmod_database = cmod_handler.database
-    result = cmod_database.get_shots_data(shotlist, sql_table=TABLE_NAME)
+    result = database.get_shots_data(shotlist, sql_table=TABLE_NAME)
     assert_frame_equal_unordered(
         result[ALL_ITERATION_COLUMNS], shot_data[ALL_ITERATION_COLUMNS]
     )
