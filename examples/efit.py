c782d8faad3d7296312cb3d74419fa2e7868c563
Joshua Lorincz <26237613+lajz@users.noreply.github.com>
2024-06-06 15:57:16 -0400

    Fix column counts

--- examples/efit.py
+++ examples/efit.py
@@ -7,9 +7,7 @@ execute a simple workflow to fetch EFIT parameters.
 from disruption_py.settings import LogSettings, ShotSettings
 from disruption_py.utils.mappings.tokamak import Tokamak
 from disruption_py.utils.mappings.tokamak_helpers import (
-    get_tokamak_from_environment,
-    get_tokamak_handler,
-)
+    get_tokamak_from_environment, get_tokamak_handler)
 
 tokamak = get_tokamak_from_environment()
 handler = get_tokamak_handler(tokamak)
@@ -17,11 +15,11 @@ handler = get_tokamak_handler(tokamak)
 if tokamak is Tokamak.D3D:
     shot_ids_request = [161228]
     run_methods = ["_get_efit_parameters"]
-    shape = (247, 15)
+    shape = (247, 16)
 elif tokamak is Tokamak.CMOD:
     shot_ids_request = [1150805012]
     run_methods = ["_get_EFIT_parameters"]
-    shape = (62, 24)
+    shape = (62, 25)
 else:
     raise ValueError(f"Unspecified or unsupported tokamak: {tokamak}.")
 
