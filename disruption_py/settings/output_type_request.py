7e62d8d5750633879622f8a1b132ca9376dc477e
Joshua Lorincz <26237613+lajz@users.noreply.github.com>
2024-06-07 05:34:06 -0700

    Jlorincz/fix pd concat (#153)
    * Fix for pd concat warning
    
    * Add safe concat
    
    * Fix result bug
    
    * Run black
    
    * Fix column number for d3d
    
    * Do not drop column
    
    * Fix column counts
    
    * Fix concat axis bug
    
    * Fix efit.py formatting

--- disruption_py/settings/output_type_request.py
+++ disruption_py/settings/output_type_request.py
@@ -12,6 +12,7 @@ import pandas as pd
 from disruption_py.databases.database import ShotDatabase
 from disruption_py.utils.mappings.mappings_helpers import map_string_to_enum
 from disruption_py.utils.mappings.tokamak import Tokamak
+from disruption_py.utils.utils import safe_df_concat
 
 
 @dataclass
@@ -249,8 +250,7 @@ class DataFrameOutputRequest(OutputTypeRequest):
         self.results: pd.DataFrame = pd.DataFrame()
 
     def _output_shot(self, params: ResultOutputTypeRequestParams):
-        if not params.result.empty and not params.result.isna().all().all():
-            self.results = pd.concat([self.results, params.result], ignore_index=True)
+        self.results = safe_df_concat(self.results, [params.result])
 
     def get_results(self, params: FinishOutputTypeRequestParams):
         return self.results
@@ -312,9 +312,7 @@ class CSVOutputRequest(OutputTypeRequest):
         if self.flexible_columns:
             if file_exists:
                 existing_df = pd.read_csv(self.filepath)
-                combined_df = pd.concat(
-                    [existing_df, params.result], ignore_index=True, sort=False
-                )
+                combined_df = safe_df_concat(existing_df, [params.result])
             else:
                 combined_df = params.result
 
@@ -357,7 +355,7 @@ class BatchedCSVOutputRequest(OutputTypeRequest):
 
     def _write_batch_to_csv(self):
         file_exists = os.path.isfile(self.filepath)
-        combined_df = pd.concat(self.batch_data, ignore_index=True, sort=False)
+        combined_df = safe_df_concat(pd.DataFrame(), self.batch_data)
         combined_df.to_csv(
             self.filepath, mode="a", index=False, header=(not file_exists)
         )
