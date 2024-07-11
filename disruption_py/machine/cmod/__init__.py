e0232798228991eedfb453087f65f33b2e66adb0
Amos Decker <amdecker9@gmail.com>
2024-07-11 10:29:28 -0400

    move hardcoding to inits

---
+++ disruption_py/machine/cmod/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from disruption_py.machine.cmod.basic import BasicCmodRequests, CModEfitRequests
+
+METHOD_HOLDERS = [BasicCmodRequests, CModEfitRequests]
--- disruption_py/machine/physics_method_holders.py
+++ disruption_py/machine/physics_method_holders.py
@@ -1,21 +1,15 @@
-from disruption_py.machine.d3d.basic import BasicD3DRequests
-from disruption_py.machine.d3d.efit import D3DEfitRequests
-from disruption_py.machine.cmod.basic import BasicCmodRequests, CModEfitRequests
+#!/usr/bin/env python3
+
 from disruption_py.machine.tokamak import Tokamak
 
 
 def get_physics_method_holders(tokamak: Tokamak):
     """Return a list of classes containing the built-in physics methods."""
     if tokamak is Tokamak.D3D:
-        return [
-            D3DEfitRequests(),
-            BasicD3DRequests(),
-        ]
+        from disruption_py.machine.d3d import METHOD_HOLDERS
+        return METHOD_HOLDERS
     elif tokamak is Tokamak.CMOD:
-
-        return [
-            CModEfitRequests(),
-            BasicCmodRequests(),
-        ]
+        from disruption_py.machine.cmod import METHOD_HOLDERS
+        return METHOD_HOLDERS
     else:
         raise ValueError(f"Invalid tokamak for physics methods {tokamak}")
