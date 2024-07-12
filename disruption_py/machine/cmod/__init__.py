202cd8e28fed2f1f4a2ae031219ca982be104f4d
gtrevisan <gtrevisan@users.noreply.github.com>
2024-07-12 11:29:22 -0400

    extract efit methods for C-MOD

--- disruption_py/machine/cmod/__init__.py
+++ disruption_py/machine/cmod/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-from disruption_py.machine.cmod.physics import BasicCmodRequests, CModEfitRequests
+from disruption_py.machine.cmod.physics import BasicCmodRequests
+from disruption_py.machine.cmod.efit import CModEfitRequests
 
 METHOD_HOLDERS = [BasicCmodRequests, CModEfitRequests]
