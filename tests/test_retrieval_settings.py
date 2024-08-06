66d686989b53f2f438dff95ad813955b262c9d0c
Amos Decker <amdecker9@gmail.com>
2024-08-06 16:56:06 -0400

    More detailed retrieval settings tests

---
+++ tests/test_retrieval_settings.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import os
+
+import pandas as pd
+import pytest
+
+from disruption_py.inout.mds import ProcessMDSConnection
+from disruption_py.machine.tokamak import Tokamak
+from disruption_py.settings.retrieval_settings import RetrievalSettings
+from disruption_py.workflow import get_shots_data
+
+
+def skip_on_fast_execution(method):
+    if "GITHUB_ACTIONS" in os.environ:
+
+        @pytest.mark.skip("fast execution")
+        def wrapper(method):
+            return method
+
+        return wrapper
+    return method
+
+
+@skip_on_fast_execution
+@pytest.mark.parametrize("num_processes", [1, 2])
+def test_sql_cache(tokamak, shotlist, num_processes):
+    """
+    Use `i_efc` to test retrieving cached data from SQL. `i_efc` exists in SQL and
+    it is the only parameter returned from its physics method, so the physics
+    method will not to run. This test uses a dummy MDSconnection to ensure we don't
+    call MDSplus.
+    """
+    # `i_efc` does not exist on DIII-D
+    if tokamak != Tokamak.CMOD:
+        pytest.skip()
+
+    retrieval_settings = RetrievalSettings(
+        cache_setting="sql",
+        use_cache_setting_timebase=True,
+        run_columns=["i_efc"],
+        run_tags=[],
+        only_requested_columns=True,
+        efit_nickname_setting="default",
+    )
+
+    def dummy_mds_initializer():
+        return ProcessMDSConnection(ProcessMDSConnection.DUMMY_CONNECTION_STRING)
+
+    results = get_shots_data(
+        tokamak=tokamak,
+        shotlist_setting=shotlist,
+        retrieval_settings=retrieval_settings,
+        num_processes=num_processes,
+        mds_connection_initializer=dummy_mds_initializer,
+    )
+    for res in results:
+        assert set(res.columns) == {"i_efc", "shot", "time", "commit_hash"}
+
+
+@skip_on_fast_execution
+@pytest.mark.parametrize("num_processes", [1, 2])
+def test_only_requested_columns(tokamak, shotlist, num_processes):
+    """
+    Ensure `only_requested_columns` work. `v_loop` is returned by
+    `get_ohmic_parameters`, so we should not see `p_oh` returned. `q95` is from
+    efit, so none of the other efit quantities should be returned.
+    """
+    retrieval_settings = RetrievalSettings(
+        run_columns=["v_loop", "q95"],
+        run_tags=[],
+        run_methods=[],
+        only_requested_columns=True,
+    )
+    results = get_shots_data(
+        tokamak=tokamak,
+        shotlist_setting=shotlist,
+        retrieval_settings=retrieval_settings,
+        num_processes=num_processes,
+    )
+    for res in results:
+        assert set(res.columns) == {"v_loop", "q95", "shot", "time", "commit_hash"}
+
+
+@pytest.fixture(scope="module")
+def full_time_domain_data(tokamak, shotlist):
+    retrieval_settings = RetrievalSettings(
+        efit_nickname_setting="default", domain_setting="full"
+    )
+    results = get_shots_data(
+        tokamak=tokamak,
+        shotlist_setting=shotlist,
+        retrieval_settings=retrieval_settings,
+        num_processes=2,
+    )
+    return results
+
+
+@skip_on_fast_execution
+@pytest.mark.parametrize("num_processes", [1, 2])
+@pytest.mark.parametrize("domain_setting", ["flattop", "rampup_and_flattop"])
+def test_domain_setting(
+    tokamak, shotlist, num_processes, domain_setting, full_time_domain_data
+):
+    """
+    Test the two partial domain settings by comparing their start and end times
+    with the full domain.
+    """
+    retrieval_settings = RetrievalSettings(
+        efit_nickname_setting="default", domain_setting=domain_setting
+    )
+    results = get_shots_data(
+        tokamak=tokamak,
+        shotlist_setting=shotlist,
+        retrieval_settings=retrieval_settings,
+        num_processes=num_processes,
+    )
+    for part_domain, full_domain in zip(results, full_time_domain_data):
+        # Both partial domains should end before the full domain, but only flattop
+        # starts after
+        if domain_setting == "flattop":
+            assert "start", part_domain["time"][0] > full_domain["time"][0]
+        assert "end", part_domain["time"].values[-1] < full_domain["time"].values[-1]
+
+
+@skip_on_fast_execution
+@pytest.mark.parametrize("num_processes", [1, 2])
+def test_output_exists(shotlist, test_file_path_f, num_processes):
+    """Test creation of all output formats."""
+    results = get_shots_data(
+        shotlist_setting=shotlist,
+        output_setting=[
+            "list",
+            "dataframe",
+            test_file_path_f(".csv"),
+            test_file_path_f(".hdf5"),
+        ],
+        num_processes=num_processes,
+    )
+
+    list_output, df_output, csv_processed, hdf_processed = results
+    # TODO dictionary, sql
+    assert isinstance(list_output, list)
+    assert isinstance(df_output, pd.DataFrame)
+    assert csv_processed == hdf_processed == len(shotlist)
+    assert os.path.exists(test_file_path_f(".csv"))
+    assert os.path.exists(test_file_path_f(".hdf5"))
