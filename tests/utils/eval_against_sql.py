a3df715eb5794aeee2c399aab9905cc84aac56c1
gtrevisan <gtrevisan@users.noreply.github.com>
2024-09-05 16:31:05 -0400

    fix consider-using-f-string

--- tests/utils/eval_against_sql.py
+++ tests/utils/eval_against_sql.py
@@ -155,11 +155,7 @@ def eval_shot_against_cache(
         expectation = "failure" if expect_failure else "success"
         failure = "failed" if data_difference.failed else "succeeded"
         logger.debug(
-            "Expected {expectation} and {failure}:\n{report}".format(
-                expectation=expectation,
-                failure=failure,
-                report=data_difference.column_mismatch_string,
-            )
+            f"Expected {expectation} and {failure}:\n{data_difference.column_mismatch_string}"
         )
     assert not data_difference.failed, "Comparison failed"
 
