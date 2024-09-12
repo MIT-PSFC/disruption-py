d8ee7ef80c5e7a33254c038e114659e6551065f5
Gregorio L. Trevisan <gtrevisan@users.noreply.github.com>
2024-09-12 07:04:53 -0700

    Fix quick examples (#302)
    * move quick example workflows into main functions
    
    * remove interactive queries from example
    
    * revamp imports for quick test

--- examples/sql.py
+++ examples/sql.py
@@ -1,59 +1,51 @@
 #!/usr/bin/env python3
 
 """
-execute a few meaningful queries to test DB connection.
+example module for SQL.
 """
 
-import os
 
 from disruption_py.machine.tokamak import Tokamak, resolve_tokamak_from_environment
 from disruption_py.workflow import get_database
 
-queries = [
-    "select count(distinct shot) from disruption_warning",
-    "select count(distinct shot) from disruption_warning"
-    + " where shot not in (select shot from disruptions)",
-    "select count(distinct shot) from disruption_warning"
-    + " where shot in (select shot from disruptions)",
-    "select count(distinct shot) from disruptions",
-]
-tokamak = resolve_tokamak_from_environment()
-db = get_database(tokamak=tokamak)
 
-if tokamak is Tokamak.D3D:
-    vals = [13245, 8055, 5190, 24219]
-elif tokamak is Tokamak.CMOD:
-    vals = [10435, 6640, 3795, 13785]
-else:
-    raise ValueError(f"Unspecified or unsupported tokamak: {tokamak}.")
+def main():
+    """
+    execute a few meaningful queries to test DB connection.
+    """
 
-print(f"Initialized DB: {db.user}@{db.host}/{db.db_name}")
+    queries = [
+        "select count(distinct shot) from disruption_warning",
+        "select count(distinct shot) from disruption_warning"
+        + " where shot not in (select shot from disruptions)",
+        "select count(distinct shot) from disruption_warning"
+        + " where shot in (select shot from disruptions)",
+        "select count(distinct shot) from disruptions",
+    ]
+    tokamak = resolve_tokamak_from_environment()
+    db = get_database(tokamak=tokamak)
 
-while queries:
+    if tokamak is Tokamak.D3D:
+        vals = [13245, 8055, 5190, 24219]
+    elif tokamak is Tokamak.CMOD:
+        vals = [10435, 6640, 3795, 13785]
+    else:
+        raise ValueError(f"Unspecified or unsupported tokamak: {tokamak}.")
 
-    query = queries.pop(0)
-    print(">", query.strip(" "))
+    print(f"Initialized DB: {db.user}@{db.host}/{db.db_name}")
 
-    out = db.query(query)
-    print("=", out.shape)
+    while queries:
 
-    print(out.iloc[0] if out.shape[0] == 1 else out)
-    if vals:
-        assert out.iloc[0, 0] == vals.pop(0)
+        query = queries.pop(0)
+        print(">", query.strip(" "))
 
-    if queries:
-        print()
-        continue
+        out = db.query(query)
+        print("=", out.shape)
 
-    if not __debug__ or any(
-        k in os.environ for k in ["PYTEST_CURRENT_TEST", "GITHUB_ACTIONS"]
-    ):
-        break
+        print(out.iloc[0] if out.shape[0] == 1 else out, "\n")
+        if vals:
+            assert out.iloc[0, 0] == vals.pop(0)
 
-    try:
-        query = input("\n> ")
-        if query:
-            queries += [query]
-    except (EOFError, KeyboardInterrupt):
-        print()
-        break
+
+if __name__ == "__main__":
+    main()
