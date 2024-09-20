0ce969b0da7bfaff9a6c9b4c93dcc6b0fa04cf2c
Amos Decker <amdecker9@gmail.com>
2024-09-20 03:53:20 -0700

    Remove attribute error

--- disruption_py/core/physics_method/runner.py
+++ disruption_py/core/physics_method/runner.py
@@ -151,7 +151,6 @@ def populate_method(
         CalculationError,
         NotImplementedError,
         ValueError,
-        AttributeError,
     ) as e:
         physics_method_params.logger.warning(
             "[Shot %s]: Failed to populate %s with error %s",
