51cf95857fea2f1845b3e721241ef1ee463f53d9
Amos Decker <amdecker9@gmail.com>
2024-09-20 06:57:32 -0400

    trigger tests

--- disruption_py/core/physics_method/runner.py
+++ disruption_py/core/physics_method/runner.py
@@ -49,7 +49,8 @@ def get_prefilled_shot_data(physics_method_params: PhysicsMethodParams):
         ).all()
     ):
         physics_method_params.logger.error(
-            "[Shot %s]: ERROR Computation on different timebase than pre-filled shot data",
+            "[Shot %s]: ERROR Computation on different timebase than pre-filled "
+            "shot data",
             physics_method_params.shot_id,
         )
     return pre_filled_shot_data
