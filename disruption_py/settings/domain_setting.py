f0e94b38da2dfe097b457b49cdfa2f27a4bcc800
gtrevisan <gtrevisan@users.noreply.github.com>
2024-09-24 07:25:04 -0400

    remove obsolete pylint exclusion

--- disruption_py/settings/domain_setting.py
+++ disruption_py/settings/domain_setting.py
@@ -102,8 +102,6 @@ class FlattopDomainSetting(DomainSetting):
         raise ValueError(f"flattop domain not defined for tokamak: {params.tokamak}")
 
     def _get_domain_cmod(self, params: DomainSettingParams) -> np.ndarray:
-        # TODO: a future PR will fix physics methods to be called without underscores.
-        # pylint: disable-next=protected-access
         ip_parameters = CmodPhysicsMethods.get_ip_parameters(
             params=params.physics_method_params
         )
@@ -205,8 +203,6 @@ class RampupAndFlattopDomainSetting(DomainSetting):
         )
 
     def _get_domain_cmod(self, params: DomainSettingParams) -> np.ndarray:
-        # TODO: a future PR will fix physics methods to be called without underscores.
-        # pylint: disable-next=protected-access
         ip_parameters = CmodPhysicsMethods.get_ip_parameters(
             params=params.physics_method_params
         )
