836ab7acae8dc3964d9304448c829a323148bebd
yumouwei <46117079+yumouwei@users.noreply.github.com>
2024-10-18 13:14:04 -0400

    q0 -> qqmagx

--- disruption_py/machine/cmod/efit.py
+++ disruption_py/machine/cmod/efit.py
@@ -36,7 +36,7 @@ class CmodEfitMethods:
         "li": r"\efit_aeqdsk:ali",
         "upper_gap": r"\efit_aeqdsk:otop/100",
         "lower_gap": r"\efit_aeqdsk:obott/100",
-        "q0": r"\efit_aeqdsk:q0",
+        "q0": r"\efit_aeqdsk:qqmagx",
         "qstar": r"\efit_aeqdsk:qstar",
         "q95": r"\efit_aeqdsk:q95",
         "v_loop_efit": r"\efit_aeqdsk:vloopt",
