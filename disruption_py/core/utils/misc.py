68d124ef401e2bf2ff82122d3df742924bb4ff34
gtrevisan <gtrevisan@users.noreply.github.com>
2025-03-11 13:34:31 -0400

    remove safe_df_concat

--- disruption_py/core/utils/misc.py
+++ disruption_py/core/utils/misc.py
@@ -14,7 +14,6 @@ from tempfile import mkdtemp
 from typing import List
 
 import numpy as np
-import pandas as pd
 
 
 def instantiate_classes(lst: List):
@@ -73,50 +72,6 @@ def safe_cast(array: np.ndarray, dtype, copy=False) -> np.ndarray:
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=RuntimeWarning)
         return array.astype(dtype, copy=copy)
-
-
-def safe_df_concat(base_df: pd.DataFrame, new_dfs: List[pd.DataFrame]) -> pd.DataFrame:
-    """
-    Safely concatenate a base DataFrame with a list of new DataFrames.
-
-    Parameters
-    ----------
-    base_df : pd.DataFrame
-        The base DataFrame to concatenate with.
-    new_dfs : List[pd.DataFrame]
-        A list of new DataFrames to concatenate.
-
-    Returns
-    -------
-    pd.DataFrame
-        The concatenated DataFrame.
-    """
-    if isinstance(new_dfs, pd.DataFrame):
-        new_dfs = [new_dfs]
-
-    all_cols = set(base_df.columns).union(*[set(new_df.columns) for new_df in new_dfs])
-
-    new_dfs = [new_df.dropna(axis=1, how="all") for new_df in new_dfs]
-    new_dfs = [
-        new_df
-        for new_df in new_dfs
-        if not new_df.empty and not new_df.isna().all().all()
-    ]
-
-    if len(new_dfs) == 0:
-        return base_df
-
-    if base_df.empty:
-        concat_df = pd.concat(new_dfs, axis=0, ignore_index=True, sort=False)
-    else:
-        concat_df = pd.concat(
-            [base_df] + new_dfs, axis=0, ignore_index=True, sort=False
-        )
-
-    missing_cols = all_cols - set(concat_df.columns)
-    for col in missing_cols:
-        concat_df[col] = np.nan
-    return concat_df
 
 
 @lru_cache
