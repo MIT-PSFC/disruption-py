7e18320360aeafd3470e2862a0e2d105e60930ad
Gregorio L. Trevisan <gtrevisan@users.noreply.github.com>
2025-04-01 13:25:17 -0700

    Ship command-line entry point with package (#425)
    * move simple workflow into package
    
    * ingest shots as positionals, allow choosing output file
    
    * remove quickstart from docs
    
    * add quickstart guide
    
    * more explicit multi-shot example
    
    * fix typo

--- disruption_py/workflow.py
+++ disruption_py/workflow.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 """
-The main entrypoint for retrieving DisruptionPy data.
+main workflow
 """
 
+import argparse
+import os
 import time
 from itertools import repeat
 from multiprocessing import Pool
@@ -15,6 +17,7 @@ from tqdm.auto import tqdm
 from disruption_py.core.retrieval_manager import RetrievalManager
 from disruption_py.core.utils.misc import (
     get_elapsed_time,
+    get_temporary_folder,
     shot_msg,
     without_duplicates,
 )
@@ -34,6 +37,7 @@ from disruption_py.settings.shotlist_setting import (
     ShotlistSettingType,
     shotlist_setting_runner,
 )
+from tests.utils.factory import get_tokamak_test_shotlist
 
 
 def _execute_retrieval(args):
@@ -219,3 +223,61 @@ def _get_mds_instance(tokamak, mds_connection_initializer):
     if mds_connection_initializer:
         return mds_connection_initializer()
     return get_mdsplus_class(tokamak)
+
+
+def run(
+    tokamak, methods, shots, efit_tree, time_base, output_file, processes, log_level
+):
+    """
+    simple workflow.
+    """
+
+    if not tokamak:
+        tokamak = resolve_tokamak_from_environment()
+    if not shots:
+        shots, *_ = get_tokamak_test_shotlist(tokamak)
+    methods = methods or None
+
+    sett = RetrievalSettings(
+        efit_nickname_setting=efit_tree, time_setting=time_base, run_methods=methods
+    )
+    out = get_shots_data(
+        tokamak=tokamak,
+        shotlist_setting=shots,
+        retrieval_settings=sett,
+        num_processes=processes,
+        log_settings=log_level,
+        output_setting="dataframe",
+    )
+
+    if output_file:
+        output_file = os.path.realpath(output_file)
+    else:
+        output_file = os.path.join(get_temporary_folder(), "output.csv")
+    logger.info("Output: {output_file}", output_file=output_file)
+    out.to_csv(output_file, index=False)
+
+    return out
+
+
+def cli():
+    """
+    simple argument parser.
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("shots", nargs="*")
+    parser.add_argument("-t", "--tokamak", type=str)
+    parser.add_argument("-m", "--methods", type=str, action="append")
+    parser.add_argument("-e", "--efit-tree", type=str, default="disruption")
+    parser.add_argument("-b", "--time-base", type=str, default="disruption_warning")
+    parser.add_argument("-o", "--output-file", type=str)
+    parser.add_argument("-p", "--processes", type=int, default=1)
+    parser.add_argument("-l", "--log-level", type=str, default="VERBOSE")
+
+    return run(**vars(parser.parse_args()))
+
+
+if __name__ == "__main__":
+    print(cli())
