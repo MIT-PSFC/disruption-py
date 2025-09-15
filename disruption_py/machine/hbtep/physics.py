4e232ffa5e4458c534ca7df0c3320fb562a9d92e
gtrevisan <gtrevisan@users.noreply.github.com>
2025-09-15 14:31:08 -0400

    add skeleton for HBT-EP

---
+++ disruption_py/machine/hbtep/physics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Module for retrieving and calculating data for HBTEP physics methods.
+"""
+
+
+class HbtepPhysicsMethods:
+    """
+    This class provides methods to retrieve and calculate physics-related data
+    for HBTEP.
+    """
