b4bce824494a1f72ff81397a5b81daa34c71b816
yumouwei <yumou.wei2013@gmail.com>
2025-09-30 13:56:24 -0400

    Implement get_n_mode_data and associated functions

--- disruption_py/machine/hbtep/physics.py
+++ disruption_py/machine/hbtep/physics.py
@@ -10,7 +10,12 @@ from scipy.integrate import cumulative_trapezoid
 from disruption_py.core.physics_method.caching import cache_method
 from disruption_py.core.physics_method.decorator import physics_method
 from disruption_py.core.physics_method.params import PhysicsMethodParams
-from disruption_py.core.utils.math import interp1
+from disruption_py.core.utils.math import (
+    butterworth_filter,
+    gaussian_low_pass_filter,
+    interp1,
+)
+from disruption_py.machine.hbtep.util import HbtepUtilMethods
 from disruption_py.machine.tokamak import Tokamak
 
 
@@ -191,3 +196,223 @@ class HbtepPhysicsMethods:
         # interpolate to requested timebase
         sxr = interp1(t_sxr, sxr, params.times, "linear")
         return {"sxr_midplane": sxr}
+
+    @staticmethod
+    @physics_method(
+        columns=[
+            "n_equal_1_mode",
+            "n_equal_1_normalized",
+            "n_equal_1_phase",
+            "n_equal_1_frequency",
+            "n_equal_2_mode",
+            "n_equal_2_normalized",
+            "n_equal_2_phase",
+            "n_equal_2_frequency",
+        ],
+        tokamak=Tokamak.HBTEP,
+    )
+    def get_n_mode_data(params: PhysicsMethodParams):
+        """
+        Get the n=1 and 2 mode amplitdes, phases, and frequencies from the toroidal array (TA)
+        # TODO: implement this function
+        """
+        # Get TA data
+        ta_data = HbtepPhysicsMethods._get_ta_data(params)
+        data = ta_data["ta_pol_data_filt"]
+        time = ta_data["ta_pol_time"]
+        phi = ta_data["ta_pol_phi"]
+        n, m = len(data), len(
+            data[0]
+        )  # TODO: decide if we need to convert data to np.ndarray
+
+        # Construct A matrix and calculate its inversion
+        A = np.zeros((n, 5))
+        A[:, 0] = np.ones(n)
+        A[:, 1] = np.sin(phi)
+        A[:, 2] = np.cos(phi)
+        A[:, 3] = np.sin(2 * phi)
+        A[:, 4] = np.cos(2 * phi)
+        Ainv = np.linalg.pinv(A)
+
+        # Use least squares to solve for coefficients
+        x = np.matmul(Ainv, data)
+        n1_amp = np.sqrt(x[1, :] ** 2 + x[2, :] ** 2)  # [T]
+        n2_amp = np.sqrt(x[1, :] ** 2 + x[2, :] ** 2)
+        n1_phase = -np.arctan2(x[1, :], x[2, :])  # *-1 to corrected the slope of phase
+        n2_phase = -np.arctan2(x[3, :], x[4, :])
+
+        # Calculate mode frequency. Note that gaussian_low_pass_filter is not a causal filter.
+        n1_phase_filt = gaussian_low_pass_filter(
+            HbtepUtilMethods.unwrap_phase(n1_phase), time, 1e-5
+        )
+        n2_phase_filt = gaussian_low_pass_filter(
+            HbtepUtilMethods.unwrap_phase(n2_phase), time, 1e-5
+        )
+        n1_freq = np.gradient(n1_phase_filt) / np.gradient(time) / (2 * np.pi)
+        n2_freq = np.gradient(n2_phase_filt) / np.gradient(time) / (2 * np.pi)
+
+        # Interpolate output data to the requested timebase
+        n1_amp = interp1(time, n1_amp, params.times, "linear")
+        n2_amp = interp1(time, n2_amp, params.times, "linear")
+        n1_phase = interp1(time, n1_phase, params.times, "linear")
+        n2_phase = interp1(time, n2_phase, params.times, "linear")
+        n1_freq = interp1(time, n1_freq, params.times, "linear")
+        n2_freq = interp1(time, n2_freq, params.times, "linear")
+
+        # Calculate normalized mode amplitudes
+        btor = HbtepPhysicsMethods.get_btor(params)["btor"]
+        n1_amp_norm = n1_amp / btor
+        n2_amp_norm = n2_amp / btor
+
+        return {
+            "n_equal_1_mode": n1_amp,
+            "n_equal_1_normalized": n1_amp_norm,
+            "n_equal_1_phase": n1_phase,
+            "n_equal_1_frequency": n1_freq,
+            "n_equal_2_mode": n2_amp,
+            "n_equal_2_normalized": n2_amp_norm,
+            "n_equal_2_phase": n2_phase,
+            "n_equal_2_frequency": n2_freq,
+        }
+
+    @staticmethod
+    @cache_method
+    def _get_ta_data(params: PhysicsMethodParams):
+        r"""
+        Get poloidal and radial field measurements from the toroidal array (TA) sensors
+        TA sensors are stored in \HBTEP2::TOP.SENSORS.MAGNETIC:[SENSOR_NAME]
+        Return TA pol and rad data in their original timebases
+        """
+        output = {}
+        # Sensor names
+        output["ta_pol_names"] = [
+            "TA01_S1P",
+            "TA01_S2P",
+            "TA01_S3P",
+            "TA02_S1P",
+            "TA02_S2P",
+            "TA02_S3P",
+            "TA03_S1P",
+            "TA03_S2P",
+            "TA03_S3P",
+            "TA04_S1P",
+            "TA04_S2P",
+            "TA04_S3P",
+            "TA05_S1P",
+            "TA05_S2P",
+            "TA05_S3P",
+            "TA06_S1P",
+            "TA06_S2P",
+            "TA06_S3P",
+            "TA07_S1P",
+            "TA07_S2P",
+            "TA07_S3P",
+            "TA08_S1P",
+            "TA08_S2P",
+            "TA08_S3P",
+            "TA09_S1P",
+            "TA09_S2P",
+            "TA09_S3P",
+            "TA10_S1P",
+            "TA10_S2P",
+            "TA10_S3P",
+        ]
+        output["ta_rad_names"] = [
+            "TA01_S2R",
+            "TA02_S2R",
+            "TA03_S2R",
+            "TA04_S2R",
+            "TA05_S2R",
+            "TA06_S2R",
+            "TA07_S2R",
+            "TA08_S2R",
+            "TA09_S2R",
+            "TA10_S2R",
+        ]
+        # Toroidal and poloidal positions of the poloidal sensors
+        output["ta_pol_phi"] = (
+            np.pi
+            / 180
+            * np.array(
+                [
+                    241.5,
+                    250.5,
+                    259.5,
+                    277.5,
+                    286.5,
+                    295.5,
+                    313.5,
+                    322.5,
+                    331.5,
+                    349.5,
+                    358.5,
+                    7.5,
+                    25.5,
+                    34.5,
+                    43.5,
+                    61.5,
+                    70.5,
+                    79.5,
+                    97.5,
+                    106.5,
+                    115.5,
+                    133.5,
+                    142.5,
+                    151.5,
+                    169.5,
+                    178.5,
+                    187.5,
+                    205.5,
+                    214.5,
+                    223.5,
+                ]
+            )
+        )
+        output["ta_pol_theta"] = (
+            np.ones(len(output["ta_pol_phi"])) * (189 - 360) * np.pi / 180
+        )
+        # Toroidal and poloidal positions of the radial sensors -- not implemented in hbteplib
+
+        # Get TA time
+        output["ta_pol_time"] = params.mds_conn.get_dims(
+            r"\TOP.SENSORS.MAGNETIC:TA01_S1P", tree_name="hbtep2"
+        )[
+            0
+        ]  # [s]
+        output["ta_rad_time"] = params.mds_conn.get_dims(
+            r"\TOP.SENSORS.MAGNETIC:TA01_S2R", tree_name="hbtep2"
+        )[
+            0
+        ]  # [s]
+        fs_ta_pol = (
+            1 / (output["ta_pol_time"][1] - output["ta_pol_time"][0])
+        ).round()  # should be 1/(2e-6)=500kHz
+        fs_ta_rad = (1 / (output["ta_rad_time"][1] - output["ta_rad_time"][0])).round()
+
+        # Get TA data
+        ta_pol_data_raw, ta_pol_data_filt = [], []
+        for sensor_name in output["ta_pol_names"]:
+            sensor_data_raw = params.mds_conn.get_data(
+                r"\TOP.SENSORS.MAGNETIC:" + sensor_name, tree_name="hbtep2"
+            )  # TODO: unit
+            sensor_data_filt = butterworth_filter(
+                sensor_data_raw, fs=fs_ta_pol, cutoff=2e3, order=2, btype="high"
+            )
+            ta_pol_data_raw.append(sensor_data_raw)
+            ta_pol_data_filt.append(sensor_data_filt)
+        ta_rad_data_raw, ta_rad_data_filt = [], []
+        for sensor_name in output["ta_rad_names"]:
+            sensor_data_raw = params.mds_conn.get_data(
+                r"\TOP.SENSORS.MAGNETIC:" + sensor_name, tree_name="hbtep2"
+            )  # TODO: unit
+            sensor_data_filt = butterworth_filter(
+                sensor_data_raw, fs=fs_ta_rad, cutoff=2e3, order=2, btype="high"
+            )
+            ta_rad_data_raw.append(sensor_data_raw)
+            ta_rad_data_filt.append(sensor_data_filt)
+        output["ta_pol_data_raw"] = ta_pol_data_raw
+        output["ta_pol_data_filt"] = ta_pol_data_filt
+        output["ta_rad_data_raw"] = ta_rad_data_raw
+        output["ta_rad_data_filt"] = ta_rad_data_filt
+
+        return output
