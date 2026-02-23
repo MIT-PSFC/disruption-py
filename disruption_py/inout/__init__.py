f217240a76b6631dd0370c11951cdcf81212ac39
Sameer Chaturvedi <sameerc@mit.edu>
2026-02-23 00:22:53 -0500

    Add DataConnection and ProcessConnection ABCs
      Introduce abstract base classes for the backend abstraction layer.
      DataConnection defines the per-shot data access interface (get_data,
      get_data_with_dims, get_dims, cleanup). ProcessConnection defines
      the per-process factory interface (get_shot_connection, from_config).
    
    Signed-off-by: Sameer Chaturvedi <sameerc@mit.edu>

---
+++ disruption_py/inout/__init__.py
@@ -0,0 +1,5 @@
+"""Data connection abstractions and implementations."""
+
+from disruption_py.inout.base import DataConnection, ProcessConnection
+
+__all__ = ["DataConnection", "ProcessConnection"]
