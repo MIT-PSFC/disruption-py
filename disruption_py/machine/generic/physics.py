934889a3ba35b5b7d239e701ce9bcb95b5f1284c
yumouwei <46117079+yumouwei@users.noreply.github.com>
2026-04-02 17:59:58 -0400

    Implement first version of get_current_quench_time

--- disruption_py/machine/generic/physics.py
+++ disruption_py/machine/generic/physics.py
@@ -13,6 +13,7 @@ from disruption_py.machine.cmod import CmodPhysicsMethods
 from disruption_py.machine.d3d import D3DPhysicsMethods
 from disruption_py.machine.east import EastPhysicsMethods
 from disruption_py.machine.mast.physics import MastPhysicsMethods
+from disruption_py.machine.generic.util import GenericUtilMethods
 from disruption_py.machine.tokamak import Tokamak
 
 
@@ -115,3 +116,116 @@ class GenericPhysicsMethods:
             time_domain[flattop_end:] = 3
 
         return {"time_domain": time_domain}
+
+    @staticmethod
+    @physics_method(columns=["current_quench_time"])
+    def get_current_quench_time(params: PhysicsMethodParams):
+        """
+        TODO: write docstring
+
+        Implement current quench time computation from Bob's scripts
+
+        Args:
+            params (PhysicsMethodParams): _description_
+        """
+        # Initialize test criteria
+        criteria = {
+            "shot_duration": lambda duration, duration_min: duration > duration_min,
+            "abs_ip_max": lambda ip_max, ip_threshold: abs(ip_max) > ip_threshold,
+            "ip0_over_ip_max": lambda ip0_over_ip_max, rampdown_threshold: ip0_over_ip_max
+            > rampdown_threshold,
+            "ip0_over_max_didt": lambda ip0_over_max_didt, tau_cq_max: ip0_over_max_didt
+            < tau_cq_max,
+            "abs_ip_final": lambda ip_final, ip_final_max: abs(ip_final) < ip_final_max,
+            "abs_ip0": lambda ip0, ip_threshold: abs(ip0) > ip_threshold,
+        }
+
+        # Get machine-specific parameters
+        if params.tokamak == Tokamak.D3D:
+            # TODO: put these in dynaconf
+            thresholds = {
+                "shot_duration": 0.5,
+                "abs_ip_max": 0.1e6,
+                "ip0_over_ip_max": 0.33,
+                "ip0_over_max_didt": 0.05,
+                "abs_ip_final": 100e3,
+                "abs_ip0": 0.1e6,
+            }
+            # Get ip and t_ip
+            try:
+                ip, t_ip = params.mds_conn.get_data_with_dims(
+                    f"ptdata('ip', {params.shot_id})"
+                )  # [A], [ms]
+                t_ip = t_ip / 1.0e3  # [ms] -> [s]
+            except mdsExceptions.MdsException as e:
+                params.logger.warning(
+                    "Failed to get measured plasma current parameters. Skip current quench time computation."
+                )
+            # Subtract baseline offset to ip
+            (baseline_indices,) = np.where(t_ip <= 0)
+            if len(baseline_indices) > 0:
+                ip_baseline = np.mean(ip[baseline_indices])
+                ip -= ip_baseline
+        elif params.tokamak == Tokamak.EAST:
+            # ip, t_ip = EastUtilMethods.retrieve_ip(params, shot_id)
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+        # Compute parameters for the tests
+        # Get duration and polarity
+        end_of_current_params = GenericUtilMethods.get_end_of_current(
+            ip=ip, ip_time=t_ip, threshold=thresholds["abs_ip_max"]
+        )
+        duration = end_of_current_params["duration"]
+        polarity = end_of_current_params["polarity"]
+
+        ip_upright = ip * polarity
+
+        # Find the maximum plasma current excluding the current spike
+        (time_indices,) = np.where((t_ip > 0) & (t_ip < duration - 0.050))
+        ip_max = max(ip_upright[time_indices]) * polarity
+
+        # Find the plasma current right before the end of the discharge
+        (time_indices,) = np.where((t_ip > duration - 0.06) & (t_ip < duration - 0.04))
+        if len(time_indices) == 0:
+            raise ValueError(
+                "Invalid timebase for shot {param.shot_id}. Cannot compute current quench time."
+            )
+        candidate_ip0 = np.mean(ip_upright[time_indices]) * polarity
+
+        # Compute dI/dt during the latter part of the discharge
+        (time_indices,) = np.where((t_ip > duration - 0.05) & (t_ip < duration + 0.05))
+        dIdt_upright = np.diff(ip_upright[time_indices]) / diff(t_ip[time_indices])
+        indx = np.argmin(dIdt_upright)
+        candidate_max_didt = dIdt_upright[indx] * polarity
+        candidate_t_disrupt = t_ip[time_indices[indx]]
+
+        # Compute ip_final
+        (time_indices,) = np.where(
+            (t_ip > candidate_t_disrupt) & (t_ip < candidate_t_disrupt + 0.15)
+        )
+        ip_final = min(ip_upright[time_indices])
+
+        # Collect the computed parameters for the tests
+        parameters = {
+            "shot_duration": duration,
+            "abs_ip_max": ip_max,
+            "ip0_over_ip_max": candidate_ip0 / ip_max if ip_max != 0 else None,
+            "ip0_over_max_didt": (
+                candidate_ip0 / candidate_max_didt if candidate_max_didt != 0 else None
+            ),
+            "abs_ip_final": abs(ip_final),
+            "abs_ip0": abs(candidate_ip0),
+        }
+
+        # Run all tests
+        for test, criterion in criteria.items():
+            parameter, threshold = parameters[test], thresholds[test]
+            if all(x is not None for x in (parameter, threshold)) and not criterion(
+                parameter, threshold
+            ):
+                # Failed any of the 6 test criteria. Mark the shot as non-disruptive.
+                return {"current_quench_time": [np.nan]}
+
+        return {"current_quench_time": [candidate_t_disrupt]}
---
+++ disruption_py/machine/generic/util.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+"""
+Module for helper, not physics, methods.
+"""
+
+import numpy as np
+import scipy
+
+
+class GenericUtilMethods:
+    """
+    A class of helper methods that might fetch and compute data from MDSplus
+    but are not physics methods.
+    """
+
+    @staticmethod
+    def get_end_of_current(ip, ip_time, threshold=1e5):
+        """
+        Python implementation of end_of_current_d3d.m and other variants
+
+        Args:
+            ip (_type_): _description_
+            ip_time (_type_): _description_
+            threshold (_type_): _description_
+
+        Returns {duration, polarity}
+        """
+        # Determine if there was any finite plasma current on this shot.  If
+        # not, then the shot was a "no plasma" shot, and the end-of-shot is set
+        # to 0 s.
+        (finite_indices,) = np.where((ip_time >= 0) & (abs(ip) > threshold))
+        if len(finite_indices) == 0:
+            return {"duration": 0, "ip_max": 0, "polarity": 1}
+        dt = np.diff(ip_time)
+        duration = sum(dt(finite_indices[:-1]))
+        if duration < 0.1:
+            # Assume < 100 ms is not a bona fide plasma
+            return {"duration": 0, "ip_max": 0, "polarity": 1}
+
+        # Determine the polarity of the plasma current.
+        polarity = np.sign(np.trapz(ip[finite_indices], ip_time[finite_indices]))
+        ip_upright = ip * polarity
+
+        # Find all the times that Ip is greater than the threshold.  The largest
+        # time value is the end of current.  But also check to see if the plasma
+        # current signal has been digitized for long enough to capture the end of
+        # the discharge.  If not, negate the end of shot value to indicate that
+        # that actual value cannot be determined, but it is longer than
+        # abs(value).
+        (indices,) = np.where((ip_upright >= threshold) & (ip_time > 0))
+        # Get the last index
+        max_idx = indices[-1] if len(indices) > 0 else None
+        duration = ip_time(max_idx)
+        if max_idx == len(ip_time) - 1:
+            duration = -duration  # TODO: what is this for?
+
+        # Find Ip_max (with correct polarity)
+        ip_max = max(ip_upright) * polarity
+
+        return {"duration": duration, "polarity": polarity}
