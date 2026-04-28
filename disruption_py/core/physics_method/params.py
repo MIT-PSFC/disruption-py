635e35c9d3b52d30fda4d65e38401c6aec433e92
gtrevisan <gtrevisan@users.noreply.github.com>
2026-04-28 16:59:26 -0400

    offer data getters from params itself

--- disruption_py/core/physics_method/params.py
+++ disruption_py/core/physics_method/params.py
@@ -69,3 +69,42 @@ class PhysicsMethodParams:
             },
             dim="idx",
         )
+
+    def get_data(self, *args, **kwargs) -> np.ndarray:
+        """
+        Get data using the data connection.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments for `data_conn.get_data`.
+        **kwargs
+            Keyword arguments for `data_conn.get_data`.
+        """
+        return self.data_conn.get_data(*args, **kwargs)
+
+    def get_data_with_dims(self, *args, **kwargs) -> np.ndarray:
+        """
+        Get data with dimensions using the data connection.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments for `data_conn.get_data_with_dims`.
+        **kwargs
+            Keyword arguments for `data_conn.get_data_with_dims`.
+        """
+        return self.data_conn.get_data_with_dims(*args, **kwargs)
+
+    def get_dims(self, *args, **kwargs) -> np.ndarray:
+        """
+        Get dimensions using the data connection.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments for `data_conn.get_dims`.
+        **kwargs
+            Keyword arguments for `data_conn.get_dims`.
+        """
+        return self.data_conn.get_dims(*args, **kwargs)
