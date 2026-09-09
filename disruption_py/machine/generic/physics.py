901ff2744e09f53c40f2c2301eec9cd5633a6b54
gtrevisan <gtrevisan@users.noreply.github.com>
2026-09-09 14:49:17 -0400

    prefer pylint disable-next to disable

--- disruption_py/machine/generic/physics.py
+++ disruption_py/machine/generic/physics.py
@@ -66,14 +66,14 @@ class GenericPhysicsMethods:
         }
         # Get data and threshold parameters
         if params.tokamak == Tokamak.CMOD:
-            # pylint: disable=import-outside-toplevel, cyclic-import
+            # pylint: disable-next=import-outside-toplevel, cyclic-import
             from disruption_py.machine.cmod.physics import CmodPhysicsMethods
 
             ip_parameters = CmodPhysicsMethods.get_ip_parameters(params=params)
             signals["dipprog_dt"] = ip_parameters["dipprog_dt"]
             signals["ip_prog"] = ip_parameters["ip_prog"]
         elif params.tokamak == Tokamak.D3D:
-            # pylint: disable=import-outside-toplevel, cyclic-import
+            # pylint: disable-next=import-outside-toplevel, cyclic-import
             from disruption_py.machine.d3d.physics import D3DPhysicsMethods
 
             ip_parameters = D3DPhysicsMethods.get_ip_parameters(params=params)
@@ -82,13 +82,13 @@ class GenericPhysicsMethods:
             signals["ip_prog"] = ip_parameters["ip_prog"]
             signals["power_supply_railed"] = psr_parameter["power_supply_railed"]
         elif params.tokamak == Tokamak.EAST:
-            # pylint: disable=import-outside-toplevel, cyclic-import
+            # pylint: disable-next=import-outside-toplevel, cyclic-import
             from disruption_py.machine.east.physics import EastPhysicsMethods
 
             ip_parameters = EastPhysicsMethods.get_ip_parameters(params=params)
             signals["dipprog_dt"] = ip_parameters["dipprog_dt"]
         elif params.tokamak == Tokamak.MAST:
-            # pylint: disable=import-outside-toplevel, cyclic-import
+            # pylint: disable-next=import-outside-toplevel, cyclic-import
             from disruption_py.machine.mast.physics import MastPhysicsMethods
 
             ip_parameters = MastPhysicsMethods.get_ip_parameters(params=params)
@@ -196,7 +196,7 @@ class GenericPhysicsMethods:
                 ip_baseline = np.mean(ip[baseline_indices])
                 ip -= ip_baseline
         elif params.tokamak == Tokamak.EAST:
-            # pylint: disable=import-outside-toplevel, cyclic-import
+            # pylint: disable-next=import-outside-toplevel, cyclic-import
             from disruption_py.machine.east.util import EastUtilMethods
 
             ip, t_ip = EastUtilMethods.retrieve_ip(params, params.shot_id)
